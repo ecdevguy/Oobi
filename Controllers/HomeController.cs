6ab50ba58e6f777d2308fc624cea7a5a50914258
Elias Cleveland <eliascleveland1@outlook.com>
2023-02-04 22:32:47 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using Oobi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oobi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string length, string grade, string subject)
+        {
+            ChatGPT chatGPT = new ChatGPT();
+            string response = chatGPT.ApiCall(length, grade, subject);
+            
+            return View(response);
+        }
+    }
+}
