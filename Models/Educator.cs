a86f0af26eeee6097a9e3d9ae558d30fa00f21ab
Elias Cleveland <eliascleveland1@outlook.com>
2023-03-03 11:10:54 +0800

    Input check added to Controller/Model

--- Models/Educator.cs
+++ Models/Educator.cs
@@ -72,6 +72,56 @@ namespace Oobi.Models
         {
             return _educatorId;
         }
+        public static Grade GetGradeFromString(string input)
+        {
+            switch (input.ToUpper())
+            {
+                case "A":
+                    return Grade.A;
+                case "B":
+                    return Grade.B;
+                case "C":
+                    return Grade.C;
+                case "D":
+                    return Grade.D;
+                case "F":
+                    return Grade.F;
+                default:
+                    throw new ArgumentException("Invalid grade input.");
+            }
+        }
+        public static Subject GetSubjectFromString(string input)
+        {
+            switch (input.ToLower())
+            {
+                case "art":
+                    return Subject.Art;
+                case "history":
+                    return Subject.History;
+                case "english":
+                    return Subject.English;
+                case "mandarin":
+                    return Subject.Mandarin;
+                case "science":
+                    return Subject.Science;
+                default:
+                    throw new ArgumentException("Invalid subject.");
+            }
+        }
+        public static Length GetLengthFromString(string input)
+        {
+            switch (input)
+            {
+                case "short":
+                    return Length.Short;
+                case "medium":
+                    return Length.Medium;
+                case "long":
+                    return Length.Long;
+                default:
+                    throw new ArgumentException("Invalid length.");
+            }
+        }
     }
 
 }
