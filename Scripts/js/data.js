7df3fb2eba5f2fe9b023d42b60bffe64ce4f138b
ameza <andrew@meza.design>
2023-03-15 01:18:18 -0400

    Add functionality on successful ajax call

--- Scripts/js/data.js
+++ Scripts/js/data.js
@@ -1,12 +1,20 @@
 jQuery($ => {
 	const $form = $("#form");
-	$form.submit(function(e) {
+	// Used to determine if the form has been submitted initially
+	let submitted = false;
+	// When submitt button is clicked
+	$form.submit(function (e) {
+		// Prevent the form from refreshing the page
 		e.preventDefault();
+		// Remove focus from submit button
+		$('#submit').blur();
+		// Set variables for input values
 		const
 			name = $form.find('#name').val(),
 			subject = $form.find('#subject input:checked').val(),
 			grade = $form.find('#grade input:checked').val(),
 			length = $form.find('#length input:checked').val();
+		// Request responses from back-end
 		$.ajax({
 			type: $form.attr('method'),
 			url: $form.attr('action'),
@@ -18,6 +26,15 @@ jQuery($ => {
 			},
 			dataType: 'json',
 			success: function (response) {
+				submitted = true;
+				// Reset name field
+				$('#name').val('');
+				// Check for initial submission
+				if (submitted) {
+					$('#submit').val($('#submit').data('value'));
+					$('#submit').removeAttr('data-value');
+				}
+				// Add responses to cards
 				var children = document.querySelector('#responses').children;
 				for (let i = 0; i < response.data.length; i++) {
 					let text = response.data[i];
@@ -26,6 +43,7 @@ jQuery($ => {
 				}
 			}
 		});
+		// Prevent the form from refreshing the page
 		return false;
 	});
 });
