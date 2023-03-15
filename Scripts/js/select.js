c96ff41986f0afcae6994203d58048e03218e0bc
ameza <andrew@meza.design>
2023-03-15 02:26:27 -0400

    Add form validation and checks

--- Scripts/js/select.js
+++ Scripts/js/select.js
@@ -2,5 +2,12 @@ const radios = document.querySelectorAll('#form .select input[type="radio"] + la
 radios.forEach((radio) => {
   radio.addEventListener('click', () => {
       radio.parentNode.previousElementSibling.textContent = radio.textContent;
+      radio.parentNode.parentNode.parentNode.classList.remove('error');
   });
+});
+const nameInput = document.querySelector('#name');
+nameInput.addEventListener('keyup', () => {
+    if (nameInput.classList.contains('error') && nameInput.nodeValue != '') {
+        nameInput.classList.remove('error');
+    }
 });
