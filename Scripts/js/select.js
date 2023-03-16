310701a64e20f66850a2f893d7414e25e5147524
ameza <andrew@meza.design>
2023-03-15 22:41:42 -0400

    Add error messages and submission show/hide

--- Scripts/js/select.js
+++ Scripts/js/select.js
@@ -1,13 +1,20 @@
 const radios = document.querySelectorAll('#form .select input[type="radio"] + label');
 radios.forEach((radio) => {
-  radio.addEventListener('click', () => {
-      radio.parentNode.previousElementSibling.textContent = radio.textContent;
-      radio.parentNode.parentNode.parentNode.classList.remove('error');
-  });
+    radio.addEventListener('click', () => {
+        const field = radio.parentNode.parentNode.parentNode;
+        radio.parentNode.previousElementSibling.textContent = radio.textContent;
+        field.classList.remove('error');
+        field.nextSibling.remove();
+    });
 });
 const nameInput = document.querySelector('#name');
 nameInput.addEventListener('keyup', () => {
     if (nameInput.classList.contains('error') && nameInput.nodeValue != '') {
         nameInput.classList.remove('error');
+        nameInput.nextSibling.remove();
     }
+});
+const acceptTermsInput = document.querySelector('#acceptTerms');
+acceptTermsInput.nextElementSibling.addEventListener('click', () => {
+    acceptTermsInput.nextElementSibling.nextElementSibling.remove();
 });
